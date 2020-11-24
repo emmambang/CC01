0b4020b296ba21df4fd3131bb5b82f6963233f9d
emmambang <74076644+emmambang@users.noreply.github.com>
2020-11-24 17:40:59 +0100

    Winforms

---
+++ WindowsFormsApp1/Print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Print
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Identifiant { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Print(string nom, string prenom, string identifiant, string email, string contact, DateTime birthday)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Identifiant = identifiant;
+            Email = email;
+            Contact = contact;
+            Birthday = birthday;
+        }
+    }
+}
