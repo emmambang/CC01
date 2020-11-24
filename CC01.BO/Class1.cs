5caf1d7befec5309c9e65c50b92f93242e9b4a0e
emmambang <74076644+emmambang@users.noreply.github.com>
2020-11-24 11:37:13 +0100

    Winforms

--- CC01.BO/Class1.cs
+++ CC01.BO/Class1.cs
@@ -4,5 +4,21 @@ namespace CC01.BO
 {
     public class Class1
     {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Identifiant { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Class1(string nom, string prenom, string identifiant, string email, string contact, DateTime birthday)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Identifiant = identifiant;
+            Email = email;
+            Contact = contact;
+            Birthday = birthday;
+        }
     }
 }
